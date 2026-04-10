aaa1071c4d76d06ab5833eaf17551fb6cd0fa6a9
tahsin <tahsinblox609@gmail.com>
2026-04-09 20:14:38 -0400

    Unicafe Finished

--- Part 1/CourseInfo/src/App.jsx
+++ Part 1/CourseInfo/src/App.jsx
@@ -1,56 +1,86 @@
+import { useState } from "react";
+
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const rate = (rate) => {
+    setGood(rate);
   };
+
+  const rate2 = (rate) => {
+    setBad(rate);
+  };
+
+  const rate3 = (rate) => {
+    setNeutral(rate);
+  };
+
+  let all = good + neutral + bad;
+
+  let average = (good + bad * -1) / all;
+
+  let positive = (good / all) * 100;
 
   return (
     <div>
-      <div>
-        <Header course={course} />
-        <Content course={course} />
-        <Total course={course} />
-      </div>
+      <h1>Give FeedBack </h1>
+
+      <Button onClick={() => rate(good + 1)} text="Good" />
+      <Button onClick={() => rate3(neutral + 1)} text="Neutral" />
+      <Button onClick={() => rate2(bad + 1)} text="Bad" />
+
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+        positive={positive}
+      />
     </div>
   );
 };
-export default App;
 
-function Header(props) {
-  return <h1>{props.course.name}</h1>;
-}
+const Button = (props) => {
+  return <button onClick={props.onClick}> {props.text} </button>;
+};
 
-function Content(props) {
+const StatisticLine = ({ text, value }) => {
   return (
-    <>
-      <div>
-        {props.course.parts[0].name} {props.course.parts[0].exercises}
-      </div>
-      <div>
-        {props.course.parts[1].name} {props.course.parts[1].exercises}
-      </div>
-      <div>
-        {props.course.parts[2].name} {props.course.parts[2].exercises}
-      </div>
-    </>
-  );
-}
-
-const Total = (props) => {
-  return (
-    <div>{`Total: ${props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}`}</div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
+
+const Statistics = ({ all, average, positive, good, bad, neutral }) => {
+  if (all == 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p1>No Feed Back Given</p1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="Good" value={good} />
+          <StatisticLine text="Neutral" value={neutral} />
+          <StatisticLine text="Bad" value={bad} />
+          <StatisticLine text="All" value={all} />
+          <StatisticLine text="Average" value={average.toFixed(1)} />
+          <StatisticLine text="Positive" value={positive.toFixed(1) + "%"} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
