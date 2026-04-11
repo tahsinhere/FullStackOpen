7e065af409b94d8ca1838341a67ebcf8bd6513ee
tahsin <tahsinblox609@gmail.com>
2026-04-10 20:25:13 -0400

    Done with Part2a

--- Part 1/CourseInfo/src/App.jsx
+++ Part 1/CourseInfo/src/App.jsx
@@ -1,52 +1,56 @@
-import { useState } from "react";
+import Course from "./components/Course";
 
 const App = () => {
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
   ];
-
-  const [selected, setSelected] = useState(0);
-  const [vote, setVote] = useState([2, 3, 5, 6, 7, 4, 2, 1]);
-  const switching = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length));
-    console.log(selected);
-  };
-
-  const addVote = () => {
-    const copy = [...vote];
-    copy[selected] += 1;
-    setVote(copy);
-  };
-  const findMax = () => {
-    let largest = 0;
-    for (let i = 0; i < vote.length; i++) {
-      if (vote[i] > largest) {
-        largest = i;
-      }
-    }
-    return largest;
-  };
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <div>{anecdotes[selected]} </div>
-      {`
-      This has ${vote[selected]} votes!  `}
-      <div>
-        <button onClick={addVote}>vote</button>
-        <button onClick={switching}>Next Anecdote</button>
-      </div>
-      <h1>Anecdote with most Votes </h1>
-      {anecdotes[findMax()]}
-      {`      This has ${vote[findMax()]} votes!  `}
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   );
 };
---
+++ Part 1/CourseInfo/src/components/Course.jsx
@@ -0,0 +1,32 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = (props) => <h1>{props.name}</h1>;
+
+const Content = (props) => (
+  <div>
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return <p>Total Sum {total}</p>;
+};
+export default Course;
